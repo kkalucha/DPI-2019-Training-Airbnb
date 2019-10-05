9a58c4e1148e9f78347f05f26e0a65eb9732b113
kkalucha <kanavkalucha@gmail.com>
2019-10-05 16:21:25 -0400

    tech demo

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+def predict(neighborhood, room_type, guests):
+    return neighborhood + " " + room_type + " " + guests
 
 @app.route('/', methods=['GET'])
 def main():
     return render_template('index.html')
     
-@app.route('/rec/<query>')
+@app.route('/rec', methods=['POST'])
 def recommendation():
-    sqft = request.args.get('sqft', '')
-    return render_template('recommendation.html')
+    return jsonify({'rec' : predict(request.form['neighborhood'], request.form['room_type'], request.form['guests'])})
+    #return jsonify({'rec' : '$110:'})
+    
+if __name__ == '__main__':
+    app.run()
